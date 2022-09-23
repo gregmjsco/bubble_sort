410a30b2cf6cee1682ae2655aa0d4cbd274f2693
Greg Johnston <gregmj.sco@gmail.com>
2022-09-23 16:42:09 +0900

    Update bubble sort

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,5 +1,23 @@
+#One of the simpler (but more processor-intensive) ways of sorting a group of items in an array is bubble sort, where each element is compared to the one next to it and they are swapped if the one on the left is larger than the one on the right. This continues until the array is eventually sorted.
+
+#Take in an array into the function
+#Arrange the elements of the array into order from smallest to largest 
+#Starting with the first two elements, if the one of the left is larger than the one of the right, then swap them. 
+#Move along the array an element at a time until all have been checked. 
+#Once completed, go back to the start of the array and do it again 
+#Iterate through the array until it no longer needs to be sorted
+
+require 'pry-byebug'
+
 def bubble_sort (data)
-    data.each_with_index do |value, index|
-        
-    return sorted_data
+    swapped = false
+    array_unsorted = data.length 
+    array_sorted = []
+
+    for i in i = 1
+
+    binding.pry
+
 end
+
+bubble_sort([4,3,78,2,0,2])
